0ff8ef3b79eec2790bd72226410d0d844de07322
Josef Jehlička <jehlijos@cvut.cz>
2024-03-31 20:25:32 +0200

    more GRASS tables fromats conv

--- main.py
+++ main.py
@@ -2,16 +2,38 @@ import os
 
 
 def conversion():
-    table_path = r"input_tables\grass\elevation"
+    table_path = r"input_tables\grass\differences"
     output_path = r"output_tables\qgis\new_table.qml"
 
     # Create the directories if they do not exist
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
+    color_dict = {
+        'black': "0 0 0",
+        'yellow': "255 255 0",
+        'orange': "255 128 0",
+        'red': "255 0 0",
+        'magenta': "255 0 255",
+        'indigo': "0 128 255",
+        'cyan': "0 255 255",
+        'green': "0 255 0",
+        'white': "255 255 255",
+        'grey': "128 128 128",
+        'blue': "0 0 255",
+        'brown': "180 77 25",
+        'violet': "128 0 255",
+    }
+
     # Open the table
     table = open(table_path, "r")
     table_lines = table.readlines()
     table.close()
+
+    # replace color names with rgb values
+    for i, line in enumerate(table_lines):
+        for color in color_dict:
+            if color in line:
+                table_lines[i] = line.replace(color, color_dict[color])
 
     # Create the output table
     output_table = open(output_path, "a")
@@ -25,15 +47,22 @@ def conversion():
         output_table.write(qml_header.readline())
 
     # Write the table
-    val = 1
+
     for line in table_lines:
         if line.strip():  # Check if line is not empty
+            # replace : with space
+            line = line.replace(":", " ")
             parts = line.split()
             percantage = parts[0]
+
+            # refromat percantage from 40% to 0.4
+            if "%" in percantage:
+                percantage = int(percantage[:-1]) / 100
+
             rgb_values = tuple(map(int, parts[1:]))
             hex_color = '#{:02x}{:02x}{:02x}'.format(*rgb_values)
-            output_table.write("  " + f'<paletteEntry value="{val}" color="{hex_color}" label="{percantage}"/>' + "\n")
-            val += 1
+            output_table.write(
+                "  " + f'<paletteEntry value="{percantage}" color="{hex_color}" label="{percantage}"/>' + "\n")
 
     # Add the footer - the rest of the qml_form.txt from line 6
     for line in qml_header:
--- output_tables/qgis/new_table.qml
+++ output_tables/qgis/new_table.qml
@@ -3,12 +3,9 @@
 <rasterrenderer opacity="1" alphaBand="0" band="1" type="paletted">
   <rasterTransparency/>
   <colorPalette>
-  <paletteEntry value="1" color="#00bfbf" label="0%"/>
-  <paletteEntry value="2" color="#00ff00" label="20%"/>
-  <paletteEntry value="3" color="#ffff00" label="40%"/>
-  <paletteEntry value="4" color="#ff7f00" label="60%"/>
-  <paletteEntry value="5" color="#bf7f3f" label="80%"/>
-  <paletteEntry value="6" color="#c8c8c8" label="100%"/>
+  <paletteEntry value="0.0" color="#0000ff" label="0.0"/>
+  <paletteEntry value="0" color="#ffffff" label="0"/>
+  <paletteEntry value="1.0" color="#ff0000" label="1.0"/>
   </colorPalette>
 </rasterrenderer>
 </qgis>
