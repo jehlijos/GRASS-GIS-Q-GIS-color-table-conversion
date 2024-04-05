9281756c455f070228e02633332f6ab3e7b02859
Josef Jehlička <jehlijos@cvut.cz>
2024-04-05 12:42:58 +0200

    all str_colors in dict

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import os
 
 
-def conversion():
-    table_path = r"input_tables\grass\differences"
-    output_path = r"output_tables\qgis\new_table.qml"
-
+def conversion(table_path, output_path):
     # Create the directories if they do not exist
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
     color_dict = {
+        'aqua': "100 128 255",
         'black': "0 0 0",
-        'yellow': "255 255 0",
-        'orange': "255 128 0",
-        'red': "255 0 0",
-        'magenta': "255 0 255",
-        'indigo': "0 128 255",
-        'cyan': "0 255 255",
-        'green': "0 255 0",
-        'white': "255 255 255",
-        'grey': "128 128 128",
         'blue': "0 0 255",
         'brown': "180 77 25",
+        'cyan': "0 255 255",
+        'gray': "128 128 128",
+        'grey': "128 128 128",
+        'green': "0 255 0",
+        'indigo': "0 128 255",
+        'magenta': "255 0 255",
+        'orange': "255 128 0",
+        'red': "255 0 0",
         'violet': "128 0 255",
+        'purple': "128 0 255",
+        'white': "255 255 255",
+        'yellow': "255 255 0",
     }
 
     # Open the table
-    table = open(table_path, "r")
-    table_lines = table.readlines()
-    table.close()
+    with open(table_path, 'r') as table:
+        table_lines = table.readlines()
 
     # replace color names with rgb values
     for i, line in enumerate(table_lines):
@@ -36,41 +35,38 @@ def conversion():
                 table_lines[i] = line.replace(color, color_dict[color])
 
     # Create the output table
-    output_table = open(output_path, "a")
-    # Clear the file
-    output_table.truncate(0)
+    with open(output_path, 'w') as output_table:
+        qml_header = open("qml_form.txt", "r")
 
-    qml_header = open("qml_form.txt", "r")
+        # Write the header as first 5 lines from the qml_form.txt
+        for i in range(5):
+            output_table.write(qml_header.readline())
 
-    # Write the header as first 5 lines from the qml_form.txt
-    for i in range(5):
-        output_table.write(qml_header.readline())
+        # Write the table
+        for line in table_lines:
+            if line:  # Check if line is not empty
+                # replace : with space
+                line = line.replace(":", " ")
+                parts = line.split()
+                percantage = parts[0]
 
-    # Write the table
+                # refromat percantage from 40% to 0.4
+                if "%" in percantage:
+                    percantage = int(percantage[:-1]) / 100
 
-    for line in table_lines:
-        if line.strip():  # Check if line is not empty
-            # replace : with space
-            line = line.replace(":", " ")
-            parts = line.split()
-            percantage = parts[0]
+                rgb_values = tuple(map(int, parts[1:]))
+                hex_color = '#{:02x}{:02x}{:02x}'.format(*rgb_values)
+                output_table.write(
+                    "  " + f'<paletteEntry value="{percantage}" color="{hex_color}" label="{percantage}"/>' + "\n")
 
-            # refromat percantage from 40% to 0.4
-            if "%" in percantage:
-                percantage = int(percantage[:-1]) / 100
+        # Add the footer - the rest of the qml_form.txt from line 6
+        for line in qml_header:
+            output_table.write(line)
 
-            rgb_values = tuple(map(int, parts[1:]))
-            hex_color = '#{:02x}{:02x}{:02x}'.format(*rgb_values)
-            output_table.write(
-                "  " + f'<paletteEntry value="{percantage}" color="{hex_color}" label="{percantage}"/>' + "\n")
-
-    # Add the footer - the rest of the qml_form.txt from line 6
-    for line in qml_header:
-        output_table.write(line)
-
-    output_table.close()
-    qml_header.close()
+        qml_header.close()
 
 
 if __name__ == '__main__':
-    conversion()
+    Input = r"input_tables\grass\corine"
+    Output = r"output_tables\qgis\new_table.qml"
+    conversion(Input, Output)
